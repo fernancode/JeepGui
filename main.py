45df5d960dc3a0ba6710db6557799ccf4847cf39
Fernando <FernandoAdelaFuente@gmail.com>
2021-03-30 14:20:00 -0700

    many many edits

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #works on desktop...and works on pi
 import kivy
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.core.text import LabelBase
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -9,6 +11,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+from kivy.properties import StringProperty
+
 
 #matplotlib
 import matplotlib.pyplot as plt
@@ -16,77 +20,150 @@ import matplotlib.pyplot as plt
 #get serial data
 #import serial.tools.list_ports
 
-# time stuff?
-from datetime import datetime
-import time
+#mgrs stuff
+import mgrs
 
-Window.fullscreen = 'auto'
 
+
+#####################################################
+## set some stuff beforehand
+#####################################################
+Window.fullscreen = 'auto' # default to fullscreen
+font_size = 30 #font size
+quantico = "quantico.regular.ttf" #set font
+mgrs_mode = False # for switching inbetween modes
+
+
+
+#####################################################
 ### how to make and add buttons - needed for AC later
-def Arm(instance):
+#####################################################
+buttons=[]
+def make_button(command,func):
+    """
+    function for adding a lot of buttons
+    """
+    btn = Button(text=command)
+    btn.bind(on_press = func)
+    buttons.append(btn)
+    return buttons
+
+def B1(instance):
     now=datetime.now()
     string = now.strftime("%H:%M:%S")+ "    DELIVERED:      Arm"
     string = {string:'out'}
     #add_messages(string)
     #device.send_data_broadcast('arm')
 
-def Disarm(instance):
+def B2(instance):
     now=datetime.now()
     string = now.strftime("%H:%M:%S")+ "    DELIVERED:      Disarm"
     string = {string:'out'}
     #add_messages(string)
     #device.send_data_broadcast('disarm')
 
-def Launch(instance):
+def B3(instance):
     now=datetime.now()
     string = now.strftime("%H:%M:%S")+ "    DELIVERED:      Launch"
     string = {string:'out'}
     #add_messages(string)
     #device.send_data_broadcast('relay_on')
 
-def Ping(instance):  
-    now=datetime.now()
-    string = now.strftime("%H:%M:%S")+ "    DELIVERED       Ping"
-    string = {string:'out'}
-    #add_messages(string)
-    #device.send_data_broadcast('ping')
+def LatLon_MGRS(instance):
+    """
+    TOGGLE DISPLAYING LAT/LON VS MGRS
+    """
+    global mgrs_mode
+    if mgrs_mode == False:
+        mgrs_mode = True
+        gps_handle.text = latlon_display_string
+
+    else:
+        mgrs_mode = False
+        gps_handle.text = mgrs_display_string
 
 
-#add buttons and name them using make button function ^^
-buttons=[]
-def make_button(command,func):
-    btn = Button(text=command)
-    btn.bind(on_press = func)
-    buttons.append(btn)
-    return buttons
+a = 1
+def update_gps(instance):
+    """
+    Update GPS info at regular intervals
+    """
 
-btn1 = make_button('B1',Arm)
-btn2 = make_button('B2',Disarm)
-btn3 = make_button('B3',Launch)
-btn4 = make_button('B4',Ping)
+    ### get serial data
+    global a
+    a += 1
+    b = str(a)
+
+    if mgrs_mode == True:
+        ## edit mgrs_mode string
+        #parse string into mgrs_mode
+        mgrs_display_string = " MGRS: " + b
+        gps_handle.text = mgrs_display_string
+
+    else:
+        ### edit LatLon string
+        latlon_display_string = " Lat/Lon: " + b
+        gps_handle.text = latlon_display_string
+
+
+
+##############################################################
+#GPS stuff
+
+##READ SERIAL DATA, UPDATE PLOT AND POSITION IN BACKGROUND
+#create the plot, add it to the boxlayout
+
+BBox = [-97.78999, -97.77930, 30.35803, 30.36665]
+fig = plt.figure(figsize=(4,2))
+fig.patch.set_facecolor('black')
+ax = fig.add_axes([0,0,1,1])
+
+lakewood_map = plt.imread("map.png")
+ax.set_xlim(BBox[0], BBox[1])
+ax.set_ylim(BBox[2], BBox[3])
+ax.imshow(lakewood_map, zorder=0, extent = BBox)
+
+gps_map= BoxLayout(orientation='vertical')
+gps_map.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+
+latlon_display_string = " "
+mgrs_display_string = " "
+
+gps_printout = Label(text=latlon_display_string, size_hint=(1, 0.15), font_name=quantico, font_size = font_size, valign='top')
+gps_printout.bind(size=gps_printout.setter('text_size'))
+gps_handle = gps_printout
+
+
+############################################################
+# ADD ALL BUTTONS, NEST BOXES, AND MAP TO FUNCTIONS
+############################################################
+
+btn1 = make_button('B1',B1)
+btn2 = make_button('B2',B2)
+btn3 = make_button('B3',B3)
+btn4 = make_button('LatLon/MGRS',LatLon_MGRS)
 btn_layout = BoxLayout(orientation='vertical',size_hint=(.15,1))
 for button in buttons: 
     btn_layout.add_widget(button)
 
-plt.plot([1,2,3,4])
-gps_map= BoxLayout(orientation='vertical')
-gps_map.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-
-gps_string = Label(text="Lat, lon, and other stuff", size_hint=(1, .15))
-gps_string.bind(size=gps_string.setter('text_size'))
-
 gps_layout = BoxLayout(orientation='vertical')
 gps_layout.add_widget(gps_map)
-gps_layout.add_widget(gps_string)
+gps_layout.add_widget(gps_printout)
+
+
+
+        
 
 
 class MyApp(App):
     def build(self):
+        Clock.schedule_interval(update_gps, 1)
         total_layout = BoxLayout(orientation='horizontal')
         total_layout.add_widget(btn_layout)
         total_layout.add_widget(gps_layout)
-
         return total_layout
+
 
 
 if __name__ == '__main__':
